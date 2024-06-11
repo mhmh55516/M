23e13aec6f1fb4b3e62826fd35ccb70ac69b2092
JohnReaJR <johnrea.njr1@gmail.com>
2024-06-11 18:40:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -71,88 +71,84 @@ fi
 netty=$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1)
 cat << EOF > /etc/M/cfg/config.json
 {
-"auth":"binary",
-"file":"/etc/M/layers/authy/auth.txt",
-"executable":"/etc/M/layers/authy/auth.sh",
-"limit_conn_single":-1,
-"limit_conn_request":-1,
-"services":[
+    "auth":"binary",
+     "file":"/etc/M/cfg/auth.txt",
+    "executable":"/root/auth.sh",
+    "banner":"linklayer by Newtoolsworks",
+    "limit_conn_single":-1,
+    "limit_conn_request":-1,
+     "services":[
 {
-"type":"httpdual",
-"cfg":{
-"Listen":"0.0.0.0:8000"
-}
+ "type":"httpdual",
+        "cfg":{
+          "Listen":"0.0.0.0:8000"
+        }
 },
-{
-"type":"tls",
-"cfg":{
-"Cert":"/etc/M/cfg/cert.pem",
-"Key":"/etc/M/cfg/key.pem",
-"Listen":"0.0.0.0:8001"
-}
+
+       {
+         "type":"tls",
+         "cfg":{
+          "Cert":"/etc/M/cfg/cert.pem",
+           "Key":"/etc/M/cfg/key.pem",
+           "Listen":"0.0.0.0:8001"
+         }
+       },
+       {
+        "type":"http",
+        "cfg":{
+          "Response":"HTTP/1.1 206 OK\r\n\r\n",
+          "Listen":"0.0.0.0:8002"
+
+        }
+       },
+	{
+	"type":"http",
+        "cfg":{
+          "Response":"HTTP/1.1 200 OK\r\n\r\n",
+          "Listen":"0.0.0.0:80"
+
+        }
+        },
+       {"type":"httptls",
+       "cfg":{
+         "Http":{
+            "Response":"HTTP/1.1 206 OK\r\n\r\n"
+         },
+         "TLS":{
+          "Cert":"/etc/M/cfg/cert.pem",
+          "Key":"/etc/M/cfg/key.pem"
+         },
+         "Listen":"0.0.0.0:8990"
+       }
+      },
+
+{"type":"httptls",
+       "cfg":{
+         "Http":{
+            "Response":"HTTP/1.1 200 OK\r\n\r\n"
+         },
+         "TLS":{
+          "Cert":"/root/cfg/cert.pem",
+          "Key":"/root/cfg/key.pem"
+         },
+         "Listen":"0.0.0.0:443"
+       }
+     
 },
-{
-"type":"http",
-"cfg":{
-"Response":"HTTP/1.1 206 OK\r\n\r\n",
-"Listen":"0.0.0.0:8002"
-}
-},
-{
-"type":"http",
-"cfg":{
-"Response":"HTTP/1.1 200 OK\r\n\r\n",
-"Listen":"0.0.0.0:80"
-}
-},
-{
-"type":"httptls",
-"cfg":{
-"Http":{
-"Response":"HTTP/1.1 206 OK\r\n\r\n"
-},
-"TLS":{
-"Cert":"/etc/M/cfg/cert.pem",
-"Key":"/etc/M/cfg/key.pem"
-},
-"Listen":"0.0.0.0:8990"
-}
-},
-{
-"type":"httptls",
-"cfg":{
-"Http":{
-"Response":"HTTP/1.1 200 OK\r\n\r\n"
-},
-"TLS":{
-"Cert":"/etc/M/cfg/cert.pem",
-"Key":"/etc/M/cfg/key.pem"
-},
-"Listen":"0.0.0.0:443"
-}
-},
-{
-"type":"udp",
-"cfg":{
-"listen":":36718",
-"exclude":"53,5300",
-"net":"$netty",
-"cert":"/etc/M/layers/cfgs/lnklyr.crt",
-"key":"/etc/M/layers/cfgs/lnklyr.key",
-"obfs":"LnkLyrVPN2k24",
-"max_conn_client":500000
-}
-},
-{
-"type":"dnstt",
-"cfg":{
-"Domain":"$domain",
-"Net":"$netty"
-}
-}
-]
-}
-}
+       {"type":"udp",
+       "cfg":{
+        "listen":":36718","exclude":"53,5300","net":"enp1s0","cert":"/root/layers/cfgs/my.crt","key":"/root/layers/cfgs/my.key","obfs":"yourpassword","max_conn_client":500000
+      }
+      },
+       
+       {"type":"dnstt",
+       "cfg":{
+         "Domain":"ns.linklayer.com",
+         "Net":"enp1s0"
+       }
+      }
+     ]
+   }
 EOF
 }
 fetcher () {
