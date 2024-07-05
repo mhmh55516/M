8635b68f625839559cee5479bb8acd635c38825b
JohnReaJR <johnrea.njr1@gmail.com>
2024-07-05 12:54:12 +0300

    Update azure.sh

--- bin/azure.sh
+++ bin/azure.sh
@@ -7,16 +7,14 @@ clear
 figlet -k LinkLayer | lolcat
 echo -e "\033[1;34m   ResleevedNet v.5 \033[0m  | \033[1;33m v.5 Release  | ResleevedNet \033[0m"
 echo -e "\033[1;36m╰═════════════════════════════════════════════════════╯\033[0m"
-echo ""
 }
 banner1
-echo -e "\033[1;33m  ⌯ Change Account Expiration Date\033[1;33m"
-echo -e "\033[1;36m•═══════════════════════════════════════════════════•\033[0m"
-echo -e "\033[1;33m・ Accounts & Expiration Date:\033[0m "
-echo ""
+echo -e "\033[1;33m Change Account Expiration Date\033[1;33m"
+echo -e "\033[1;36m────────────────────────────────────────────────────•\033[0m"
+echo -e "\033[1;33m Accounts   &   Expiration Date\033[0m "
+echo -e "\033[1;36m────────────────────────────────────────────────────•\033[0m"
 database="/etc/M/layers/authy/accounts.db"
 list_user=$(awk -F: '$3>=1000 {print $1}' /etc/passwd | grep -v nobody)
-i=0
 i=0
 unset _userPass
 while read user; do
@@ -25,32 +23,32 @@ _oP=$i
 [[ $i == [1-9] ]] && i=0$i && oP+=" 0$i"
 expire="$(chage -l $user | grep -E "Account expires" | cut -d ' ' -f3-)"
 if [[ $expire == "never" ]]; then
-echo -e "\033[1;31m[\033[1;36m$i\033[1;31m] \033[1;37m- \033[1;32m$user     \033[1;33m00/00/0000   S/DATE\033[0m"
+echo -e "\033[1;32m(\033[1;36m$i\033[1;32m) \033[1;37m \033[1;32m$user\033[0m"
 else
 databr="$(date -d "$expire" +"%Y%m%d")"
 hoje="$(date -d today +"%Y%m%d")"
 if [ $hoje -ge $databr ]; then
-_user=$(echo -e "\033[1;31m[\033[1;36m$i\033[1;31m] \033[1;37m- \033[1;32m$user\033[1;37m")
-datanormal="$(echo -e "\033[1;31m$(date -d"$expire" '+%d/%m/%Y')")"
+_user=$(echo -e "\033[1;32m(\033[1;36m$i\033[1;32m) \033[1;37m \033[1;32m$user\033[1;37m")
+datanormal="$(echo -e "\033[1;36m$(date -d"$expire" '+%d/%m/%Y')")"
 expired=$(echo -e "\033[1;31m・ Expired\033[0m")
 printf '%-62s%-20s%s\n' "$_user" "$datanormal" "$expired"
 echo "exp" >/tmp/exp
 else
-_user=$(echo -e "\033[1;31m[\033[1;36m$i\033[1;31m] \033[1;37m- \033[1;32m$user\033[1;37m")
-datanormal="$(echo -e "\033[1;33m$(date -d"$expire" '+%d/%m/%Y')")"
-ative=$(echo -e "\033[1;32m・ Valid\033[0m")
+_user=$(echo -e "\033[1;32m(\033[1;36m$i\033[1;32m) \033[1;37m \033[1;32m$user\033[1;37m")
+datanormal="$(echo -e "\033[1;36m$(date -d"$expire" '+%d/%m/%Y')")"
+ative=$(echo -e "\033[1;32m Valid\033[0m")
 printf '%-62s%-20s%s\n' "$_user" "$datanormal" "$ative"
 fi
 fi
 _userPass+="\n${_oP}:${user}"
 done <<<"${list_user}"
 tput sgr0
-echo ""
 if [ -a /tmp/exp ]; then
 rm /tmp/exp
 fi
 num_user=$(awk -F: '$3>=1000 {print $1}' /etc/passwd | grep -v nobody | wc -l)
-echo -ne "\033[1;32m・ Enter or select a user \033[1;33m[\033[1;36m1\033[1;33m-\033[1;36m$num_user\033[1;33m]\033[1;37m: "
+echo -e "\033[1;36m────────────────────────────────────────────────────•\033[0m"
+echo -ne "\033[1;32mEnter or select a user \033[1;33m(\033[1;36m1\033[1;32m-\033[1;36m$num_user\033[1;33m)\033[1;37m: "
 read option
 if [[ -z $option ]]; then
 echo ""
@@ -65,10 +63,8 @@ echo ""
 exit 1
 else
 if [[ $(grep -c /$username: /etc/passwd) -ne 0 ]]; then
-echo ""
-echo -e "\033[1;31m・EX:\033[1;33m(\033[1;32mDATE: \033[1;37mDay/Month/Year \033[1;33mOR \033[1;32mDAYS: \033[1;37m30\033[0m)"
-echo ""
-echo -ne "\033[1;32m・ New date or days for the user \033[1;33m$username: \033[1;37m"
+echo -e "\033[1;36mEX:\033[1;33m(\033[1;32mDATE: \033[1;37mDay/Month/Year \033[1;33mOR \033[1;32mDAYS: \033[1;37m30)\033[0m"
+echo -ne "\033[1;32mNew date or days for the user \033[1;33m$username: \033[1;37m"
 read inputdate
 if [[ "$(echo -e "$inputdate" | grep -c "/")" = "0" ]]; then
 udata=$(date "+%d/%m/%Y" -d "+$inputdate days")
@@ -97,15 +93,15 @@ echo "For example: 01/01/2025"
 echo ""
 exit 1
 else
+echo -ne "\033[1;32m"
 chage -E $sysdate $username
-echo ""
-echo "・ User Success $username new date: $udata "
-echo ""
+echo -e "\033[1;36mUser Success $username new date: $udata \033[0m"
+echo -e "\033[1;36m────────────────────────────────────────────────────•\033[0m"
 exit 1
 fi
 else
 echo ""
-echo "・ You have entered an invalid or non-existent date!"
+echo "You have entered an invalid or non-existent date!"
 echo "Enter a valid date in Day/Month/Year format"
 echo "For example: 01/01/2025"
 echo ""
@@ -114,7 +110,7 @@ fi
 fi
 else
 echo ""
-echo "・ You have entered an invalid or non-existent date!"
+echo "You have entered an invalid or non-existent date!"
 echo "Enter a valid date in Day/Month/Year format"
 echo "For example: 21/04/2018"
 echo ""
